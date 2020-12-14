bc086e5c470c499cc110cdaf8cc4687b4b979204
Lea Marty <leandro.marty@headwaydigital.com>
2020-12-14 13:26:56 +0100

    fix: last configs

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 
-from setuptools import setup, find_packages  # noqa: H301
+from setuptools import setup, find_packages
 
-NAME = "monei"
-VERSION = "0.1.5"
+NAME = 'Monei'
+VERSION = '0.1.5'
 # To install the library, run the following
 #
 # python setup.py install
@@ -11,20 +11,29 @@ VERSION = "0.1.5"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+REQUIRES = ['urllib3 >= 1.15', 'six >= 1.10', 'certifi', 'python-dateutil']
 
 setup(
     name=NAME,
     version=VERSION,
-    description="MONEI PYTHON SDK",
-    author="MONEI",
-    author_email="hi@monei.net",
-    url="",
-    keywords=["monei", "monei pay", "pay", "payments", "payment gateway", "python", "sdk", "rest", "api"],
+    description='MONEI PYTHON SDK',
+    author='MONEI',
+    author_email='hi@monei.net',
+    maintainer='MONEI',
+    maintainer_email='support@monei.net',
+    url='https://github.com/monei/monei-python-sdk',
+    keywords=['monei', 'monei pay', 'pay', 'payments', 'payment gateway', 'python', 'sdk', 'rest', 'api'],
     install_requires=REQUIRES,
-    packages=find_packages(exclude=["test", "tests"]),
+    packages=find_packages(exclude=['test', 'tests']),
     include_package_data=True,
-    long_description="""\
-    The MONEI SDK will allow you to interact with our API in an easy and predictable way.
-    """
+    long_description='The MONEI SDK will allow you to interact with our API in an easy and predictable way',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
+    ]
 )
