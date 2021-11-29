8fbe6f07a67087180ef119e138aeac1972d8d3d3
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2021-11-29 19:33:54 +0100

    fix versioning

--- build/release.sh
+++ build/release.sh
@@ -2,7 +2,7 @@
 echo 'Releasing new version of MONEI PYTHON SDK'
 cd ..
 rm -rf dist/*
-python3 -m pip install --user --upgrade setuptools wheel twine
+python3 -m pip install --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
 cd build
