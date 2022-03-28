074ca8eeb7730e9f4cbda129901dd5141ffe101f
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2022-03-28 18:06:31 +0200

    add missing files

--- Monei/__init__.py
+++ Monei/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 
 # import apis into sdk package
 from Monei.api.apple_pay_domain_api import ApplePayDomainApi
