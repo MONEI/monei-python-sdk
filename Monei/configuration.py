126d8bb1c364d00663ce4022b416736c8b2b0210
Dmitriy <dn@monei.com>
2023-09-19 18:47:48 +0200

    chore: release v1.2.2

--- Monei/configuration.py
+++ Monei/configuration.py
@@ -359,7 +359,7 @@ conf = Monei.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.4.2\n"\
-               "SDK Package Version: 1.2.1".\
+               "SDK Package Version: 1.2.2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
